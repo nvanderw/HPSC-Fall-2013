0069a7003d8fc98e524346b21c1f0fe1b6a9461c
Nick Vanderweit <nick.vanderweit@gmail.com>
2013-09-11 15:52:20 -0600

    Fill out main(). Still some unresolved issues.

--- lab/lab-02/matrix_multiply.c
+++ lab/lab-02/matrix_multiply.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h> // for memset
 #include <stdio.h>
+#include <errno.h>
 
 #define ASSERT(x) if(!x) return 0;
 
@@ -92,5 +93,94 @@ int test_313() {
     return 1;
 }
 
-int main() {
+/**
+ * Reads up to nelem elements from the file pointer input into the array of
+ * floats pointed to by outarray.
+ *
+ * Returns the number of elements actually read, which may be less than nelem.
+ */
+size_t load_array(FILE *input, float *outarray, size_t nelem) {
+    size_t count;
+    for(count = 0; count < nelem; count++) {
+        int result = fscanf(input, "%f ", &outarray[count]);
+        if(result < 1) break;
+    }
+
+    return count;
+}
+
+void usage(char *name, FILE *out) {
+    fprintf(out, "Usage: %s <matrix> <matrix>\n", name);
+}
+
+int main(int argc, char **argv) {
+    if(argc < 3) {
+        usage(argv[0], stderr);
+        return 1;
+    }
+
+    FILE *a_file, *b_file;
+    int m_a, n_a, m_b, n_b;
+
+    a_file = fopen(argv[1], "r");
+    if(a_file == NULL) {
+        int err = errno;
+        fprintf(stderr, "Could not open file %s: %s\n", argv[1], strerror(errno));
+        return 1;
+    }
+    b_file = fopen(argv[2], "r");
+    if(b_file == NULL) {
+        int err = errno;
+        fprintf(stderr, "Could not open file %s: %s\n", argv[2], strerror(errno));
+        return 1;
+    }
+
+    int status;
+
+    status = fscanf(a_file, "%d %d", &m_a, &n_a);
+    if(status < 2) {
+        fprintf(stderr, "Could not parse file header for %s\n", argv[1]);
+        return 1;
+    }
+
+    status = fscanf(b_file, "%d %d", &m_b, &n_b);
+    if(status < 2) {
+        fprintf(stderr, "Could not parse file header for %s\n", argv[2]);
+        return 1;
+    }
+
+    if(n_a != m_b) {
+        fprintf(stderr, "Matrix dimensions do not match.\n");
+        return 1;
+    }
+
+    float a_matrix[m_a][n_a];
+    float b_matrix[m_b][n_b];
+
+    status = load_array(a_file, &a_matrix[0][0], m_a * n_a);
+    if(status < m_a * n_a) {
+        fprintf(stderr, "Error: could not parse all entries of %s\n", argv[1]);
+        return 1;
+    }
+
+    status = load_array(b_file, &b_matrix[0][0], m_b * n_b);
+    if(status < m_b * n_b) {
+        fprintf(stderr, "Error: could not parse all entries of %s\n", argv[2]);
+        return 1;
+    }
+
+    float c_matrix[m_a][n_b];
+    matrix_multiply(&a_matrix[0][0],
+                    &b_matrix[0][0],
+                    &c_matrix[0][0],
+                    m_a, m_b, n_b);
+
+    for(size_t i = 0; i < m_a; i++) {
+        for(size_t j = 0; j < n_b; j++) {
+            printf("%f ", c_matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
