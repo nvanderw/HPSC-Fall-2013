27f06102d7d8db80505e5d9864a1a993309a64af
Nick Vanderweit <nick.vanderweit@gmail.com>
2013-09-28 20:12:34 -0600

    Oops.

--- lab/lab-03/run_tests.sh
+++ lab/lab-03/run_tests.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 sizes=`seq 1000 1000 10000`
+
 for n in $sizes; do
     python generate_matrix.py -m $n -n $n -f matrix."$n"x"$n".txt -t txt
     python generate_matrix.py -m $n -n $n -f matrix."$n"x"$n".h5 -t h5
-
-    ./io_text matrix.*.txt
-    ./io_hdf matrix.*.h5
 done
+
+./io_text matrix.*.txt
+./io_hdf matrix.*.h5
