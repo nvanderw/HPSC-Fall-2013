5a75cb5ec72ce448bc513a54e3fa0bcf50ba0e29
Nick Vanderweit <nick.vanderweit@gmail.com>
2013-10-15 10:29:27 -0600

    Add convergence handling

--- lab/lab-06-speedup/jacobi.c
+++ lab/lab-06-speedup/jacobi.c
@@ -3,29 +3,51 @@
 #include <string.h>
 #include <sys/resource.h>
 
+double sum(double *A, size_t n) {
+    double result = 0;
+    #pragma omp parallel for reduction(+:result)
+    for(size_t i = 0; i < n; i++)
+        result += A[i];
+
+    return result / n;
+}
+
 /*
  * Performs a Jacobi iteration on the given matrix.
  * Arguments:
  *      A: An n x n matrix, which will be modified in place
  *      n: Dimension of the matrix
  *      max_k: Maximum number of iterations
+ *      eps: Upper bound on error
  *
+ *  Returns: nonzero on convergence, zero otherwise
  */
-void jacobi(double *A, size_t n, int max_k) {
+int jacobi(double *A, size_t n, int max_k, double eps) {
     double B[n * n];
     memcpy(&B[0], A, sizeof(double) * n * n);
 
     double *new = A;
     double *old = &B[0];
     double *tmp;
+    
+    double error[n][n];
 
     for(int k = 0; k < max_k; k++) {
         // Update the interior values
         #pragma omp parallel for
         for(size_t i = 1; i < n - 1; i++)
-            for(size_t j = 1; j < n - 1; j++)
+            for(size_t j = 1; j < n - 1; j++) {
                 new[i * n + j] = (old[(i - 1) * n + j] + old[(i + 1) * n + j]
                                 + old[i * n + (j - 1)] + old[i * n + (j + 1)])/4;
+                error[i][j] = new[i * n + j] - old[i * n + j];
+            }
+
+        double e = sum(&error[0][0], n * n);
+        if(e < eps) {
+            if(new != A)
+                memcpy(A, new, sizeof(double) * n * n);
+            return 1;
+        }
 
         // Swap the pointers
         tmp = old;
@@ -35,6 +57,7 @@ void jacobi(double *A, size_t n, int max_k) {
 
     if(new != A)
         memcpy(A, new, sizeof(double) * n * n);
+    return 0;
 }
 
 void init_jacobi(double *A, size_t n, double left, double right, double top, double bottom) {
@@ -94,6 +117,10 @@ int main(int argc, char **argv) {
     double A[n][n];
     init_jacobi(&A[0][0], n, left, right, top, bottom);
 
-    jacobi(&A[0][0], n, max_k);
+    int convergence = jacobi(&A[0][0], n, max_k, 1e-5);
+    if(convergence)
+        printf("Converged\n");
+    else
+        printf("Did not converge\n");
     print_matrix(&A[0][0], n, n);
 }
